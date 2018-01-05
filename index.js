f3afde325c1846d5fde756af556659c6e0c52fa3
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-05 19:47:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let customerId = 0;
 let mealId = 0;
 let deliveryId = 0;
+let employeeId = 0;
 let store = {customers: [], meals: [], deliveries: []}
 
 class Customer {
@@ -13,4 +14,30 @@ class Customer {
   }
 
 
+
+  static function totalSpent() {
+
+  }
+}
+
+class Meal {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+    this.id = ++mealId;
+
+    store.meals.push(this)
+  }
+
+
+}
+
+class Delivery {
+  constructor(meal, customer) {
+    this.meal = meal;
+    this.customer = customer;
+    this.id = ++deliveryId;
+
+    store.deliveries.push(this)
+  }
 }
